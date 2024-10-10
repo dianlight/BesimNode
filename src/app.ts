346ef65b22fe46ebbf8612a3bc127b1d4e1f2e58
dianlight <lucio.tarantino@gmail.com>
2024-10-10 08:32:05 +0200

    Clean Swagger

--- src/app.ts
+++ src/app.ts
@@ -87,29 +87,29 @@ export async function app_bootstrap(port = 3001, migrate = true) {
         description: 'Testing the Fastify swagger API',
         version: '0.1.0'
       },
-      servers: [
-        {
-          url: 'http://localhost:3000',
-          description: 'Development server'
-        }
-      ],
-      tags: [
-        { name: 'user', description: 'User related end-points' },
-        { name: 'code', description: 'Code related end-points' }
-      ],
-      components: {
-        securitySchemes: {
-          apiKey: {
-            type: 'apiKey',
-            name: 'apiKey',
-            in: 'header'
-          }
-        }
-      },
-      externalDocs: {
-        url: 'https://swagger.io',
-        description: 'Find more info here'
-      }
+//      servers: [
+//        {
+//          url: 'http://localhost:3000',
+//          description: 'Development server'
+//        }
+//      ],
+      // tags: [
+      //   { name: 'user', description: 'User related end-points' },
+      //   { name: 'code', description: 'Code related end-points' }
+      // ],
+      // components: {
+      //   securitySchemes: {
+      //     apiKey: {
+      //       type: 'apiKey',
+      //       name: 'apiKey',
+      //       in: 'header'
+      //     }
+      //   }
+      // },
+      // externalDocs: {
+      //   url: 'https://swagger.io',
+      //   description: 'Find more info here'
+      // }
     }
   });
 
